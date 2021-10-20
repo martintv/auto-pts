872ad4fbb8e2681771773019782c88e3644df9e0
Tverdal, Martin <martin.tverdal@nordicsemi.no>
2021-10-20 14:08:52 +0200

    trying test for alexes test

--- wid/gatt.py
+++ wid/gatt.py
@@ -373,8 +373,7 @@ def hdl_wid_25(desc):
     MMI.parse_description(desc)
 
     pts_chrc_uuid = MMI.args[0]
-    pts_chrc_handles = [int(MMI.args[1], 16), int(MMI.args[2], 16),
-                        int(MMI.args[3], 16), int(MMI.args[4], 16)]
+    pts_chrc_handles = [int(MMI.args[i],16) for i in range(1, len(MMI.args))]
 
     iut_start_handle = None
     iut_end_handle = None
